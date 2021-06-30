6180fb58f61fa8da18b889cbb9c6186cdc7c4241
Evan <evanwcc86@gmail.com>
2021-06-29 21:01:42 -0400

    just change cv pdf

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = ({toggleSidebar}) => {
                     <li><Link to="/skills"><span>S</span>kills</Link></li>
                     <li><Link to="/contact"><span>C</span>ontact</Link></li>
                     <button><a href="https://drive.google.com/file/d/1ubbrB_c6GhheOPE5qnPKyOAi5bOAjRnD/view?usp=sharing">Download CV</a></button>
-                                    
+                    
                 </ul>
                 <button onClick={toggleSidebar} className="menuIcon">
                     <i className="fas fa-bars"></i>
