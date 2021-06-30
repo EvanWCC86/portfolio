bc1fb93323a4cd9dd01035d3f3cf28103bd34190
Evan <evanwcc86@gmail.com>
2021-06-30 15:23:31 -0400

    add the news-app project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,16 +1,22 @@
 export const projectsData = [
     {
-        id:1,
+        id:"1",
         websiteUrl:"https://restaurant-demo1.netlify.app",
         
         name:"Restaurant Demo",
         skilles:["React", "Sass"]
     },
     {
-        id:2,
+        id:"2",
         websiteUrl:"https://www.evanwebrecipes.com",
         name:"Recipes website",
         skilles:["React", "material-ui"]
     },
+    {
+        id:"3",
+        websiteUrl:"https://news-demo.vercel.app",
+        name: "News-demo",
+        skilles:["NextJS, sass"]
+    }
     
 ]
