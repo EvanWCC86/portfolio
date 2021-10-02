aa1b1f8f64de86151a0055d7870dc88094589c2a
Evan <evanwcc86@gmail.com>
2021-10-01 22:20:42 -0400

    fix again

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
-import PhoneIcon from '@mui/icons-material/Phone';
-import EmailIcon from '@mui/icons-material/Email';
-import HomeIcon from '@mui/icons-material/Home';
+// import PhoneIcon from '@mui/icons-material/Phone';
+// import EmailIcon from '@mui/icons-material/Email';
+// import HomeIcon from '@mui/icons-material/Home';
+import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 
 const ContactPage = () => {
     return (
@@ -10,7 +13,7 @@ const ContactPage = () => {
             <div className="contact_container">
                 <div className="contact-info">
                     <div className="infoItem">
-                        <button><HomeIcon /></button>
+                        <button><HomeOutlinedIcon /></button>
                         <div>
                             <span>555 first st</span>
                             <span>Montreal,Quebec</span>
@@ -18,11 +21,11 @@ const ContactPage = () => {
                         </div>
                     </div>
                     <div className="infoItem">
-                        <button><PhoneIcon /></button>
+                        <button><PhoneOutlinedIcon /></button>
                         <span>514-555-5555</span>
                     </div>
                     <div className="infoItem">
-                        <button><EmailIcon /></button>
+                        <button><EmailOutlinedIcon /></button>
                         <span>realestate@gmail.com</span>
                     </div>
                 </div>
